692732827a47597f941c61ece4eadc5b06690fe6
geniehyeon31 <hgenie3113@gmail.com>
2022-08-18 21:40:52 +0900

    hj_01

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-var server = app.listen(80, function(){
+var server = app.listen(3000, function(){
     console.log("Express server has started on port 3000")
 });
 app.use(express.static('public'));
