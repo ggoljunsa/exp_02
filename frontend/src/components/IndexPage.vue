712b9bad88f4ba052962941c5b5b1198cfb004d0
quart-cavestory <jangminjun6014@gmail.com>
2022-08-19 19:24:47 +0900

    vue-change-all

---
+++ frontend/src/components/IndexPage.vue
@@ -0,0 +1,31 @@
+<template>
+    <div v-if="user">
+        <h1>접속한 유저</h1>
+        <p>아이디:{{user.id}}</p>
+        <p>비밀번호{{user.password}}</p>
+        <p>이름:{{user.name}}</p>
+        <p>12ㅇ럼ㅇㄴ렘ㄴ얼야ㅐㅁㄴ럼앨</p>
+    </div>
+    
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            user:null,
+        };
+    },
+    created(){
+        this.$http.get('/api/login')
+            .then((res) =>{
+                const user = res.data.user;
+                if(user) this.user = user;
+            })
+        .catch((err) =>{
+            console.error(err);
+        });
+    }}
+</script>
+
+<style></style>
---
+++ frontend/src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+
+Vue.prototype.$http = axios //vue 컴포넌트에서 this.$http로 요청할 수 있게 된다. 
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
